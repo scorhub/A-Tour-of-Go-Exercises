a07df99226b06ac96cdcc8fdc699e3cbf3e40c24
scor <kimmo.kuohula@gmail.com>
2021-08-30 13:24:39 +0300

    My solution

--- exercise-loops-and-functions/exercise-loops-and-functions.go
+++ exercise-loops-and-functions/exercise-loops-and-functions.go
@@ -1,10 +1,24 @@
 package main
 
+// A Tour of Go
+// Exercise: Loops and Functions
+// https://tour.golang.org/flowcontrol/8
+
 import (
 	"fmt"
 )
 
 func Sqrt(x float64) float64 {
+	var result float64
+	z := x / 2
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		if z == result {
+			return result
+		}
+		result = z
+	}
+	return result
 }
 
 func main() {
