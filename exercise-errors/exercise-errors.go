d53a5226db7c0be4b0e90a97b8fa87138ecbe060
scor <kimmo.kuohula@gmail.com>
2021-08-30 14:53:53 +0300

    Exercise: Errors - my answer

--- exercise-errors/exercise-errors.go
+++ exercise-errors/exercise-errors.go
@@ -1,11 +1,33 @@
 package main
 
+// A Tour of Go
+// Exercise: Errors
+// https://tour.golang.org/methods/20
+
 import (
 	"fmt"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("Cannot Sqrt negative number: %v", float64(e))
+}
+
 func Sqrt(x float64) (float64, error) {
-	return 0, nil
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	var result float64
+	z := x / 2
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		if z == result {
+			return result, nil
+		}
+		result = z
+	}
+	return result, nil
 }
 
 func main() {
