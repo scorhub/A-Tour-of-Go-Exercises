56597917f6c73356dcd509fcd0b0431815d9a62b
scor <kimmo.kuohula@gmail.com>
2021-08-30 14:25:15 +0300

    Exercise: Stringers - my answer

---
+++ exercise-stringers/exercise-stringers.go
@@ -0,0 +1,24 @@
+package main
+
+// A Tour of Go
+// Exercise: Stringers
+// https://tour.golang.org/methods/18
+
+import "fmt"
+
+type IPAddr [4]byte
+
+// TODO: Add a "String() string" method to IPAddr.
+func (i IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", i[0], i[1], i[2], i[3])
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
